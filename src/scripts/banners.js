c0dedaecb1be3cb2d990ae19165ec9ca8efcacfe
Jonas Krogh Fischer <jonasfiskekrogh@hotmail.com>
2023-06-10 18:24:25 +0200

    hej

--- src/scripts/banners.js
+++ src/scripts/banners.js
@@ -60,9 +60,9 @@ function showCards() {
   const brand = document.getElementById("brandSelect").value;
 
   // Sæt titlen og undertitlen
-  document.getElementById("brandTitle").textContent = brand || "All brands";
+  document.getElementById("brandTitle").textContent = brand || "Alle mærker";
   document.getElementById("kategoriSubtitle").textContent =
-    kategori || "All categories";
+    kategori || "Alle kategorier";
 
   cards.forEach((card) => {
     // Vis kort, der matcher filteret
